cd676a451fce139607d28d37c52ef9f1681c5035
chuangmaster <masterchuang@urtody.com>
2020-10-08 11:39:19 +0800

    加入專案檔案。

---
+++ SpecFlowSandbox/DateTimeHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpecFlowSandbox
+{
+    public class DateTimeHelper
+    {
+        public bool IsChristmasEve(DateTime date)
+        {
+            if (date.Month == 12 && date.Day == 24)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
